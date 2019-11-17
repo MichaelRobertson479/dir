dddc7bd9f8c7e2518e1767257476d5408561a7de
Michael Robertson <mrobertson00@stuy.edu>
2019-11-16 23:33:49 -0500

    e

--- dir.c
+++ dir.c
@@ -30,7 +30,7 @@ int main() {
         else if (file->d_type == DT_REG) {
             
             struct stat info;
-            strcat("sample/",file->d_name)
+            strcat("sample/",file->d_name);
             //stat(strcat("sample/",file->d_name),&info);
 
             size += info.st_size;
