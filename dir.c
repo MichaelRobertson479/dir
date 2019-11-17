32aeb8b8f6aafb512cd751d20f977b4ec6989140
Michael Robertson <mrobertson00@stuy.edu>
2019-11-17 00:27:12 -0500

    e

--- dir.c
+++ dir.c
@@ -9,50 +9,9 @@
 
 int main() {
 
-    // DIR *dir = opendir("sample");
+    DIR *dir = opendir("sample");
 
-    // printf("stats for sample directory:\n");
-
-    // if (dir == NULL) {
-    //     printf("%s\n",strerror(errno));
-    // }
-
-    // struct dirent *file = readdir(dir);
-
-    // int size = 0;
-
-    // struct stat info;
-
-    // while (file != NULL) {
-
-    //     printf("%s",file->d_name);
-
-    //     if (file->d_type == DT_DIR) {
-    //         printf(" (directory)");
-    //     }
-
-    //     else if (file->d_type == DT_REG) {
-
-    //         char dir_name[20];
-
-    //         strcpy(dir_name, "sample/");
-    //         stat(strcat(dir_name,file->d_name),&info);
-
-    //         size += info.st_size;
-    //     }
-
-    //     printf("\n");
-
-    //     file = readdir(dir);
-    // }
- 
-    // printf("size of regular files in directory: %d\n", size);
-
-    // closedir(dir);
-
-    DIR *dir = opendir(".");
-
-    printf("stats for current directory:\n");
+    printf("stats for sample directory:\n");
 
     if (dir == NULL) {
         printf("%s\n",strerror(errno));
@@ -60,7 +19,48 @@ int main() {
 
     struct dirent *file = readdir(dir);
 
-    long size = 0;
+    int size = 0;
+
+    struct stat info;
+
+    while (file != NULL) {
+
+        printf("%s",file->d_name);
+
+        if (file->d_type == DT_DIR) {
+            printf(" (directory)");
+        }
+
+        else if (file->d_type == DT_REG) {
+
+            char dir_name[20];
+
+            strcpy(dir_name, "sample/");
+            stat(strcat(dir_name,file->d_name),&info);
+
+            size += info.st_size;
+        }
+
+        printf("\n");
+
+        file = readdir(dir);
+    }
+ 
+    printf("size of regular files in directory: %d\n", size);
+
+    closedir(dir);
+
+    dir = opendir(".");
+
+    printf("stats for current directory:\n");
+
+    if (dir == NULL) {
+        printf("%s\n",strerror(errno));
+    }
+
+    file = readdir(dir);
+
+    size = 0;
 
     while (file != NULL) {
 
