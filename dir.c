2390becc7294f285c12b758b6f12142cffdb8d32
Michael Robertson <mrobertson00@stuy.edu>
2019-11-16 22:58:54 -0500

    e

--- dir.c
+++ dir.c
@@ -32,7 +32,7 @@ int main() {
             
             struct stat info;
 
-            stat(strcat(file->d_name,&info);
+            stat(file->d_name,&info);
 
             size += info.st_size;
         }
